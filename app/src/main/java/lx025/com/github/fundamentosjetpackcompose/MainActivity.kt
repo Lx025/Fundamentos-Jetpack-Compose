a85850925823d16476959d00834dba019040904e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-11 22:26:30 -0300

    a

--- app/src/main/java/lx025/com/github/fundamentosjetpackcompose/MainActivity.kt
+++ app/src/main/java/lx025/com/github/fundamentosjetpackcompose/MainActivity.kt
@@ -9,9 +9,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -79,9 +82,10 @@ class MainActivity : ComponentActivity() {
         }
 
     }
+
     @Composable
     fun CardMensagem(modifier: Modifier = Modifier) {
-        Row (modifier = modifier.padding(16.dp)) {
+        Row(modifier = modifier.padding(16.dp)) {
             Image(
                 painter = painterResource(id = R.drawable.avatar),
                 contentDescription = "Foto do contato",
@@ -99,20 +103,20 @@ class MainActivity : ComponentActivity() {
             }
         }
 
-
-    @Preview(showBackground = true)
-    @Composable
-    fun PreviewFuncaoInicial() {
-        FundamentosJetpackComposeTheme {
-            Inicial()
+        @Preview(showBackground = true)
+        @Composable
+        fun PreviewFuncaoInicial() {
+            FundamentosJetpackComposeTheme {
+                Inicial()
+            }
         }
-    }
 
-    @Preview(showBackground = true)
-    @Composable
-    fun PreviewNotificationBage() {
-        FundamentosJetpackComposeTheme {
-            NotificationBage()
+        @Preview(showBackground = true)
+        @Composable
+        fun PreviewNotificationBage() {
+            FundamentosJetpackComposeTheme {
+                NotificationBage()
+            }
         }
     }
 }
