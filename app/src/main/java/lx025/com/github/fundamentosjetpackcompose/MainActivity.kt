83fbacb4c724859cc78f5c721f679e73e666af0b
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-18 22:13:08 -0300

    Aula 18/08

--- app/src/main/java/lx025/com/github/fundamentosjetpackcompose/MainActivity.kt
+++ app/src/main/java/lx025/com/github/fundamentosjetpackcompose/MainActivity.kt
@@ -4,18 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -108,7 +114,9 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun PreviewFuncaoInicial() {
         FundamentosJetpackComposeTheme {
-            Inicial()
+            //Inicial()
+            //TesteModifier()
+            TesteButtom()
         }
     }
 
@@ -117,6 +125,55 @@ class MainActivity : ComponentActivity() {
     fun PreviewNotificationBage() {
         FundamentosJetpackComposeTheme {
             NotificationBage()
+        }
+    }
+
+    @Composable
+    fun TesteModifier() {
+        Column {
+            Text(
+                text = "Item 1",
+                modifier = Modifier
+                    .border(border = BorderStroke(width = 1.dp, color = Color.Green))
+                    .padding(horizontal = 8.dp)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Item 1",
+                modifier = Modifier
+                    .border(border = BorderStroke(width = 1.dp, color = Color.Green))
+                    .padding(horizontal = 8.dp, vertical = 8.dp)
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = "Item 1",
+                modifier = Modifier
+
+                    .border(border = BorderStroke(width = 1.dp, color = Color.Green))
+                    .padding(horizontal = 8.dp)
+            )
+
+        }
+
+    }
+
+    @Composable
+    fun TesteButtom() {
+        Button(
+            onClick = { /* TODO */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
+            colors = ButtonDefaults.buttonColors(
+                containerColor = Color.Green,
+                contentColor = Color.Black
+            ),
+            shape = RoundedCornerShape(4.dp)
+        ) {
+            Text(text = "Confirmar")
         }
     }
 }
